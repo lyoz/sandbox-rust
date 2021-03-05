7ac324423d7e9f3811ad3718faaf5b9dccb69692
lyoz <lyoz@users.noreply.github.com>
2021-03-06 02:22:52 +0900

    return Result<(), String>

--- chapter3/bitonic-sorter/src/second.rs
+++ chapter3/bitonic-sorter/src/second.rs
@@ -1,10 +1,18 @@
 use super::SortOrder;
 
-pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) {
-  match *order {
-    SortOrder::Ascending => do_sort(x, true),
-    SortOrder::Descending => do_sort(x, false),
-  };
+pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) -> Result<(), String> {
+  if x.len().is_power_of_two() {
+    match *order {
+      SortOrder::Ascending => do_sort(x, true),
+      SortOrder::Descending => do_sort(x, false),
+    };
+    Ok(())
+  } else {
+    Err(format!(
+      "The length of x is not a power of two. (x.len(): {}))",
+      x.len()
+    ))
+  }
 }
 
 fn do_sort<T: Ord>(x: &mut [T], up: bool) {
@@ -44,21 +52,21 @@ mod tests {
   #[test]
   fn sort_u32_ascending() {
     let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
-    sort(&mut x, &Ascending);
+    assert_eq!(sort(&mut x, &Ascending), Ok(()));
     assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
   }
 
   #[test]
   fn sort_u32_descending() {
     let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
-    sort(&mut x, &Descending);
+    assert_eq!(sort(&mut x, &Descending), Ok(()));
     assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
   }
 
   #[test]
   fn sort_str_ascending() {
     let mut x = vec!["Rust", "is", "fast", "and", "memory", "efficient", ".", "."];
-    sort(&mut x, &Ascending);
+    assert_eq!(sort(&mut x, &Ascending), Ok(()));
     assert_eq!(
       x,
       vec![".", ".", "Rust", "and", "efficient", "fast", "is", "memory"]
@@ -68,10 +76,16 @@ mod tests {
   #[test]
   fn sort_str_descending() {
     let mut x = vec!["Rust", "is", "fast", "and", "memory", "efficient", ".", "."];
-    sort(&mut x, &Descending);
+    assert_eq!(sort(&mut x, &Descending), Ok(()));
     assert_eq!(
       x,
       vec!["memory", "is", "fast", "efficient", "and", "Rust", ".", "."]
     );
+  }
+
+  #[test]
+  fn sort_to_fail() {
+    let mut x = vec![10, 30, 11];
+    assert!(sort(&mut x, &Ascending).is_err());
   }
 }
